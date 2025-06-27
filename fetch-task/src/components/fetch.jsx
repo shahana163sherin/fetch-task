47d8a1e2b6ace82989771bc68d121e331e77481d
shahana163sherin <shahanavakkarath@gmail.com>
2025-06-27 17:10:40 +0530

    Initial commit

---
+++ fetch-task/src/components/fetch.jsx
@@ -0,0 +1,28 @@
+
+import { useEffect,useState } from "react";
+function Fetch(){
+    const [state,setState]=useState([])
+
+
+    useEffect(()=>{
+      fetch("https://dummyjson.com/users")
+             .then(res => res.json())
+              .then(data => setState(data.users))
+            //    console.log(data))
+
+               
+            .catch(error=>console.error(error))
+    },[])
+
+    return(
+        <>
+       <ul>
+        {state.map(users=>(<li key={users.id}>{users.firstName}  {users.lastName}
+            {users.maidenName}  {users.age}
+        </li>))}
+       </ul>
+        </>
+    );
+
+}
+export default Fetch
